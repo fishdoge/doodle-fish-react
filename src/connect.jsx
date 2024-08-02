cb63ae45f7da7bc3cbd4c1841cc0ad2f8d3dae23
LI-YONG-QI <a2369028@gmail.com>
2024-08-02 17:35:23 +0800

    chore: tailwind css

---
+++ src/connect.jsx
@@ -0,0 +1,6 @@
+const Connect = () => {
+    return <div className="text-3xl font-bold underline text-red">Connect</div>;
+};
+
+export default Connect;
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Connect from "./connect.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/connect",
+        element: <Connect />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
+
