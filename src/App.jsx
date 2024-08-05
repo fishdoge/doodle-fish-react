6aa3ece7db1c4e60f1bf605a39f1c98e7d63f618
kevinshark <chile109@gmail.com>
2024-08-05 22:03:03 +0800

    fix: page wallet integrate

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useRef } from "react";
 import { PhaserGame } from "./game/PhaserGame";
+import { TonConnectButton } from "@tonconnect/ui-react";
+import { useTonAddress } from '@tonconnect/ui-react';
 
 function App() {
     //  References to the PhaserGame component (game and scene are exposed)
     const phaserRef = useRef();
+    const userFriendlyAddress = useTonAddress();
 
     // Event emitted from the PhaserGame component
     const currentScene = (scene) => {
         console.log(scene);
     };
 
+
     return (
         <div id="app">
+            <TonConnectButton />
+            <p>Address: {userFriendlyAddress}</p>
             <PhaserGame ref={phaserRef} currentActiveScene={currentScene} />
         </div>
     );
