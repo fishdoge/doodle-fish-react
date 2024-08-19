4b2373c31d805be8f60461b05c81f0a00d6101ae
LI-YONG-QI <a2369028@gmail.com>
2024-08-19 13:49:53 +0800

    delete file

--- backend/firesbase/userInit.js
+++ backend/firesbase/userInit.js
@@ -34,8 +34,6 @@ async function updateUserData(id, score, token) {
 async function updateInvitee(id, newInvitee) {
   const { uid, user } = await viweUserInfoByid(id);
 
-  console.log(user);
-
   const docRef = doc(database, "doodlePlayer", uid);
   let updatedInviteeArray = [];
   if (user.invitee && Array.isArray(user.invitee)) {
@@ -46,28 +44,6 @@ async function updateInvitee(id, newInvitee) {
   await updateDoc(docRef, {
     invitee: updatedInviteeArray,
   });
-}
-
-async function addRandomUsers(num) {
-  const names = [
-    "Alice",
-    "Bob",
-    "Charlie",
-    "Diana",
-    "Evan",
-    "Fiona",
-    "George",
-    "Hannah",
-    "Ian",
-    "Julia",
-  ];
-
-  for (let a = 0; a < num; a++) {
-    const randemNum = Math.floor(Math.random() * 1000);
-    const randemID = Math.floor(Math.random() * 100000);
-    const ran = randemNum % 10;
-    createUserData(names[ran], randemID, randemNum, randemNum);
-  }
 }
 
 export { createUserData, updateUserData, updateInvitee };
